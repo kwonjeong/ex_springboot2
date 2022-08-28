793bf4520f2388530838002d8a4b966163b66008
kwonjeong <kwonjeong9470@gmail.com>
2022-08-28 18:36:31 +0900

    import jsonpath

--- src/test/java/com/example/ex_springboot2/HelloControllerTest.java
+++ src/test/java/com/example/ex_springboot2/HelloControllerTest.java
@@ -7,14 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -25,7 +21,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception{
+    public void hello_return() throws Exception{
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -34,13 +30,13 @@ public class HelloControllerTest {
     }
 
     @Test
-    public void helloDto가_리턴된다() throws Exception{
+    public void helloDto_return() throws Exception{
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(
                 get("/hello/dto")
-                        .param("name", name)
+                        .param("name", (name))
                         .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
